0014e9b067d1ec3897b8ddba7cb821f32e030fbf
HelenOne <h.bright@yandex.ru>
2025-05-27 15:47:55 +0300

    added RequireAuth wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
+import RequireAuth from './components/RequireAuth.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={
+          <RequireAuth>
+            <MainPage />
+          </RequireAuth>
+        }
+      />
       <Route path="/login" element={<LoginPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
